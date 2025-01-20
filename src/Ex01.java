f29b21647730b02710b934b2dab6f6ce0c5d7ed5
落合咲慧 <sae.ochiai@rakus-partners.co.jp>
2025-01-20 10:20:57 +0900

    first commit

---
+++ src/Ex01.java
@@ -0,0 +1,8 @@
+public class Ex01 {
+    public static void main(String[] args) {
+        String message = "I have a dream today";
+        int stringLength = message.length();
+        System.out.println(stringLength);
+    }
+
+}
